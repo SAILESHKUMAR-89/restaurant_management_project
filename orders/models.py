9325b8fbb9a4fa9c8a133b85cb8d18cb3ce6cc10
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-06 00:22:16 +0530

    17964

--- orders/models.py
+++ orders/models.py
@@ -15,7 +15,7 @@ class Order(models.Model):
     items = models.ManyToManyField(MenuItem)
     total_amount = models.DecimalField(max_digits=8, decimal_places=2)
     status = models.CharField(max_length=10, choices=ORDER_STATUS_CHOICES, default='PENDING')
-    created_at = models.DateTimeField(auto_new_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return f"Order #{self.id} - {self.customer.username}"
