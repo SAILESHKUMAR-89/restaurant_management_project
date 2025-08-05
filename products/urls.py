e4218d53f059c5cc4bdd023af666743019792dc1
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-06 00:16:37 +0530

    17966

--- products/urls.py
+++ products/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from .views import HardcodedMenuAPIView
 
 urlpatterns = [
     path('items/', ItemView.as_view(), name='item-list'),
+    path('menu/', HardcodedMenuAPIView.as_view(), name = 'hardcoded-menu'),
 ]
