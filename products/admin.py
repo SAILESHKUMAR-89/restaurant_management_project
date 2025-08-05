ea1f764525b9d3f7e2d203a5f5fde65b50ce6160
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-06 00:27:32 +0530

    17965

--- products/admin.py
+++ products/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import *
+from .models import MenuItem
 
 
 # Custom Admins
@@ -9,3 +9,4 @@ class ItemAdmin(admin.ModelAdmin):
 
 # Register your models here.
 admin.site.register(Item,ItemAdmin)
+admin.site.register(MenuItem)
