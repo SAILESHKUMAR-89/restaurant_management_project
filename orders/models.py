1cc75932d62214b77cfdeda847f23c1d9fe01c55
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-06 00:04:49 +0530

    17966

--- orders/models.py
+++ orders/models.py
@@ -13,3 +13,9 @@ ORDER_STATUS_CHOICES = [
 class Order(models.Model):
     customer = models.ForeignKey(User, on_delete=models.CASCADE)
     items = models.ManyToManyField(MenuItem)
+    total_amount = models.DecimalField(max_digits=8, decimal_places=2)
+    status = models.CharField(max_length=10, choices=ORDER_STATUS_CHOICES, default='PENDING')
+    created_at = models.DateTimeField(auto_new_add=True)
+
+    def __str__(self):
+        return f"Order #{self.id} - {self.customer.username}"
