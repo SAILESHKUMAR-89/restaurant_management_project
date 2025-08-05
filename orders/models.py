a36899516f30a9b24be3ea02b1f188e3255b690f
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-05 23:59:32 +0530

    17963

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from django.contrib.auth.models import User
+from products.models import MenuItem
 
-# Create your models here.
+ORDER_STATUS_CHOICES = [
+    ('PENDING', 'Pending'),
+    ('CONFIRMED', 'Confirmed'),
+    ('PREPARING', 'Preaparing'),
+    ('DELIVERED', 'delivered'),
+    ('CANCELLED', 'cancelled'),
+]
+
+class Order(models.Model):
+    customer = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(MenuItem)
