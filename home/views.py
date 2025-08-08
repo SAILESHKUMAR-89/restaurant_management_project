41b4e2f69b748338899434dd2895d6669038aa26
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-08 23:03:06 +0530

    23052

--- home/views.py
+++ home/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.conf import settings
 
 def homepage(request):
-    return render(request, 'home/menu.html')
+    return render(request, 'home/index.html', {
+        'restaurant_name': settings.RESTAURANT_NAME
+    })
