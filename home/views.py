7db0624fc470f4fb658a5317e44932a33aa379d9
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-14 23:33:57 +0530

    30865

--- home/views.py
+++ home/views.py
@@ -5,3 +5,5 @@ def homepage(request):
     return render(request, 'home/index.html', {
         'restaurant_name': settings.RESTAURANT_NAME
     })
+def contact_view(request):
+    return render(request, "contact.html", {"restaurant_name": "Sailesh's Bistro"})
