68460694e74faf0fdd6667a91ebe9341c34669c0
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-22 23:13:53 +0530

    36282

--- products/admin.py
+++ products/admin.py
@@ -5,7 +5,8 @@ from .models import RestaurantLocation
 # Custom Admins
 class ItemAdmin(admin.ModelAdmin):
     list_display = ['item_name','item_price','created_at']
-
+class MenuAdmin(admin.ModelAdmin):
+    list_display = ("name", "price")
 
 # Register your models here.
 admin.site.register(Item,ItemAdmin,Menu)
