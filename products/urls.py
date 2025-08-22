1b9c3bdc7d102dca93662713d8f6c8def5159b06
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-22 23:07:16 +0530

    36283

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from .views import HardcodedMenuAPIView
-from .views import feedback_view
+from .views import feedback_view, contact_view
 urlpatterns = [
     path('api/menu/', HardcodedMenuAPIView.as_view(), name = 'menu-api'),
     path('feedback/', feedback_view, name='feedback'),
+    path("contact/", contact_view, name="contact"),
 ]
