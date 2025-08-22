f9f950351fe23cc0c3883042b737addec0209413
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-23 00:15:37 +0530

    36277

--- products/models.py
+++ products/models.py
@@ -42,3 +42,11 @@ class RestaurantInfo(models.Model):
 
     def __str__(self):
         return self.name
+
+class MenuItem(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_length=6, decimal_places=2)
+
+    def __str__(self):
+        return self.name
