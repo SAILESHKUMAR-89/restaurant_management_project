ba108d81f4ad5d1c5cec55c1f779102e6cd083f9
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-22 23:02:08 +0530

    36283

--- products/PY forms.py
+++ products/PY forms.py
@@ -1,8 +1,13 @@
 from django import forms
-from .models import Feedback
+from .models import Feedback, ContactSubmission
+
 
 class FeedbackForm(forms.ModelForm):
     class Meta:
         model = Feedback
         fields = ['name', 'email', 'comments']
         
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = ContactSubmission
+        fields = ['name', 'email']
