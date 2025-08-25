c6f9e568dab1167cc7dc32c468695a7058aeb24f
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-25 23:22:35 +0530

    38001

--- products/models.py
+++ products/models.py
@@ -48,6 +48,7 @@ class MenuItem(models.Model):
     name = models.CharField(max_length=100)
     description = models.TextField()
     price = models.DecimalField(max_length=6, decimal_places=2)
+    image = models.ImageField(upload_to="menu_images/", blank=True, null=True)
 
     def __str__(self):
         return self.name
