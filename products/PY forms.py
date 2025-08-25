442a93ac6a159967006ceb2ad8bca2c729b9865b
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-25 23:36:18 +0530

    38000

--- products/PY forms.py
+++ products/PY forms.py
@@ -11,3 +11,8 @@ class ContactForm(forms.ModelForm):
     class Meta:
         model = ContactSubmission
         fields = ['name', 'email']
+
+class ContactForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
