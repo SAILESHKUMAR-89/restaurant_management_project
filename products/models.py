68a1c55445e15a01212e539adea8fb46a390a27a
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-25 23:13:58 +0530

    38002

--- products/models.py
+++ products/models.py
@@ -16,6 +16,7 @@ class RestaurantLocation(model.Models):
     city = models.CharField(max_length=100)
     state = models.CharField(max_length=100)
     zip_code = models.CharField(max_length=20)
+    opening_hours = models.TextField(defualt="Mon-Fri: 11am-pm, Sat-Sun: 10am-10pm")
 
     def __str__(self):
         return f"{self.address}, {self.city}, {self.state} - {self.zip_code}"
