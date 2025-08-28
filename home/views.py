7ab3e95aac862cfd118554fb293b15de3d297f46
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-28 21:41:59 +0530

    39293

--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.conf import settings
+from .forms import ContactForm
 
 def homepage(request):
     cart = request.session.get("cart", [])
@@ -15,4 +16,13 @@ def homepage(request):
         'restaurant_phone': settings.restaurant_phone
     })
 def contact_view(request):
+    if request.method == "POST":
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            name = form.cleaned_data['name']
+            email = form.cleaned_data['email']
+            message = form.cleaned_data['message']
+             return render(request, "contact_success.html", {'name':name})
+    else:
+        form = ContactForm()
     return render(request, "contact.html", {"restaurant_name": "Sailesh's Bistro"})
