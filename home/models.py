88ca766a647c2f8fb33d205fca72b7703e732a80
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-28 21:29:18 +0530

    39296

--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Restaurant(models.Model):
+    name = models.CharField(max_length=100)
+    phone = models.CharField(max_length=20)
+    logo = models.ImageField(upload_to="logo/", blank=True, null=True)
