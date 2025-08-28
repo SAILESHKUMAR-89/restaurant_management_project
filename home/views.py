59157d6d88ecabb57b6fe1838a79b71507f19361
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-08-28 21:36:18 +0530

    39294

--- home/views.py
+++ home/views.py
@@ -2,6 +2,14 @@ from django.shortcuts import render
 from django.conf import settings
 
 def homepage(request):
+    cart = request.session.get("cart", [])
+    cart_count = len(cart)
+
+    context = {
+        "restaurant_name":"My Restaurant",
+        "restaurant_phone":"123-456-7890",
+        "cart_count":cart_count,
+    }
     return render(request, 'home/index.html', {
         'restaurant_name': settings.RESTAURANT_NAME,
         'restaurant_phone': settings.restaurant_phone
