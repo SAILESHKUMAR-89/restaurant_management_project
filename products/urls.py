77282898ebae5f2ed211ecd7eea84568761e0518
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-09-02 15:51:34 +0530

    40273

--- products/urls.py
+++ products/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("contact/", contact_view, name="contact"),
     path("menu/", menu_view, name="menu"),
     path("contact/success/", contact_success, name="contact_success"),
+    path("faq/", views.faq, name="faq"),
 ]
