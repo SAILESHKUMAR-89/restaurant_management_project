60f27dfe0cdcb58fb84da404983edbbc0181e5e2
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-09-09 17:25:51 +0530

    42903

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def homepage(request):
+    specials = TodaySpecial.objects.all()
+    return render(request, "homepage.html", {"specials":specials})
