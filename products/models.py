370a677b6c99e35e0520e3798a459d5518275a7e
SAILESHKUMAR-89 <154727824+SAILESHKUMAR-89@users.noreply.github.com>
2025-09-09 17:10:35 +0530

    42906

--- products/models.py
+++ products/models.py
@@ -60,3 +60,10 @@ class Contact(models.Model):
 
     def __str__(self):
         return f"{self.name} {{self.email}}"
+
+from django.db import models
+
+class Feedback(models.Model):
+    name = models.CharField(max_length=100)
+    Feedback_text = models.TextField()
+    
